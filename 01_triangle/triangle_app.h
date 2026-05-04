e4cbcaccc760c7078149600a3d786c8914266e01
nnamm <t.nnamm@gmail.com>
2026-05-04 20:31:58 +0900

    WIP:01_triangle_260504

---
+++ 01_triangle/triangle_app.h
@@ -0,0 +1,9 @@
+#include "common/sample_app.h"
+
+class TriangleApp : public ISampleApp
+{
+  public:
+    virtual void OnInitialize() override{}
+    virtual void OnDrawFrame() override{}
+    virtual void OnCleanup() override{}
+};
